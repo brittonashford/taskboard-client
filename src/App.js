055f4927c777a69ba085386956432a8aec7715d6
Bret Kitchell <brittonashford@gmail.com>
2022-12-22 20:36:47 -0600

    added axios, tested API connectivity, fixed CORS issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState, useEffect, useContext } from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+    const [issues, setIssues] = useState([]);
+
+    useEffect(() => {
+        async function getIssues(){
+            const response = await axios.get('https://localhost:7283/Issue/GetAllIssues');
+            console.log(response.data);
+            setIssues(response.data);
+            console.log(issues);
+        }
+
+        getIssues();
+    }, [issues])
+
+
   return (
     <div className="App">
       <Header />
